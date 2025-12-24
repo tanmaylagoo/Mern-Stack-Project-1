39756f6b867818a124f9ce2d88114e7e729515d0
tanmaylagoo <tanmaylagoo@gmail.com>
2025-12-24 11:37:46 +0530

    feat: add mern-stack-project-1 as a local dependency and configure package.json for build and start scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from "cors";
+import path from "path"
 
 dotenv.config();
 
@@ -11,7 +12,8 @@ dotenv.config();
 
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4003
+const __dirname = path.resolve()
 
 
 
@@ -19,10 +21,12 @@ const PORT = process.env.PORT
 //.use method for making the middleware
 //this works after we hit send from postman since middleware is what is between the request from the client side and response from the server side
 
+if(process.env.NODE_ENV !=="production"){
+    app.use(cors({
+    origin:"http://localhost:5173"
+  }))
+}
 
-app.use(cors({
-  origin:"http://localhost:5173"
-}))
 app.use(express.json())//this middleware will parse JSON bodies: req.body
 
 app.use(rateLimiter)
@@ -39,6 +43,16 @@ app.use('/api/notes', notesRoutes);
 /**
  * An endpoint is a combination of a URL and HTTP method that lets the client interact with a specific resource 
  */
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*", (req, res)=>{
+  res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+
+})
+
+}
 
 connectDB().then(()=>{
   app.listen(PORT, ()=>{
